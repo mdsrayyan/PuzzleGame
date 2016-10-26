cbcdc38bab7a2d26b55105aaa8f1f6f814eab8ff
Mohammad Sirajuddin Rayyan <mds0060@gmail.com>
2016-10-26 23:22:45 +0530

    Added Lazer shooting scripts and sample scene
    Sample screen for lazer beam and its reflection

---
+++ Assets/PlayerControls.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControls : MonoBehaviour {
+	public float RotateSpeed = 10f;
+	public float MoveSpeed = 5f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Amount to Move
+		float MoveForward = Input.GetAxis("Vertical") * MoveSpeed * Time.deltaTime;
+		float MoveRotate = Input.GetAxis("Horizontal") * RotateSpeed * Time.deltaTime;
+
+
+		// Move the player
+		transform.Translate(Vector3.forward * MoveForward);
+		transform.Rotate(Vector3.up * MoveRotate);
+	}
+}
